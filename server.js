2a5a9494b22598cad28f1c2b51a0f3b38c88e6ca
‪Osama Salem‬‏ <70097854+osamasalem1@users.noreply.github.com>
2023-02-22 10:07:28 +0300

    changes

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+const path = `http://jsonplaceholder.typicode.com/users`;
+
+// Requst Method
+app.get("/api/employee", (req, res) => {
+  res.json(path);
+});
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
